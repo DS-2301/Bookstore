64d54ea5a60a2e23e5354ec04a0e9b70389d446b
Daniil Sherstnev <daniilsherstnev@Daniils-MacBook-Air.local>
2021-09-15 08:50:21 -0400

    feat(book): Created Book Card + Styling

--- src/Components/Book/Book.js
+++ src/Components/Book/Book.js
@@ -1,3 +1,26 @@
-export default function Book() {
-  return <div>book</div>;
+import "./Book.css";
+import bookIcon from "../../bookIcon.svg";
+import { useDispatch } from "react-redux";
+import { removeItem } from "../../store/books";
+
+export default function Book({ book }) {
+  const dispatch = useDispatch();
+  return (
+    <div className="bookCard">
+      <div className="topBookRow">
+        <img className="bookImage" src={bookIcon} alt={book.name} />
+        <button
+          className="deleteBook"
+          onClick={() => {
+            dispatch(removeItem(book.id));
+          }}
+        >
+          X
+        </button>
+      </div>
+      <span className="bookName">{book.name}</span>
+      <span className="bookCategory">{book.category}</span>
+      <span className="bookPrice">{book.price}$</span>
+    </div>
+  );
 }
--- src/Components/Books/Books.js
+++ src/Components/Books/Books.js
@@ -1,11 +1,14 @@
+import "./Books.css";
 import { useSelector } from "react-redux";
 import Book from "../Book/Book";
 
 export default function Books() {
   const books = useSelector((store) => store.books);
   return (
-    <div>
-      <Book />
+    <div className="booksPane">
+      {books.map((book) => {
+        return <Book book={book} key={book.id} />;
+      })}
     </div>
   );
 }
